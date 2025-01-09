ab5412ab2d69995a34bc3cbac09d3cc884dd06c3
Ikhsan Lintang Prakasa <ikhsanlintang06@gmail.com>
2025-01-09 21:11:47 +0700

    Task 2 validatetion(category,order)

---
+++ backend/src/validation/category.validation.js
@@ -0,0 +1,8 @@
+import joi from "joi";
+
+export const categoryValidation = (payload) => {
+  const schema = joi.object({
+    categoryName: joi.string().trim().required(),
+  });
+  return schema.validate(payload);
+};
